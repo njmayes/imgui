8f551794ff5af32d8679ef497c6773b2c50c0f60
Nathan Mayes <amayesingnathan123@gmail.com>
2022-05-20 17:13:40 +0100

    Added pic to linux premake

--- premake5.lua
+++ premake5.lua
@@ -24,10 +24,14 @@ project "ImGui"
 
     filter "system:windows"
         systemversion "latest"
+		
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
         
     filter "configurations:Debug"
 		runtime "Debug"
-        symbols "on"
+		symbols "on"
 
     filter "configurations:Release"
 		runtime "Release"
